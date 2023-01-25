29107e01e1c77a57d989b695f7028c33490102b8
Liam Pattinson <liampattinson@gmail.com>
2023-01-25 17:53:03 +0000

    Added try/except to version detection in case package is not installed
    This can happen if the user tries to use our package without
    installing with pip, i.e. by setting PYTHONPATH.

--- freeqdsk/__init__.py
+++ freeqdsk/__init__.py
@@ -2,7 +2,11 @@ __all__ = ["geqdsk", "aeqdsk"]
 
 from importlib.metadata import version, PackageNotFoundError
 
-__version__ = version(__name__)
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    # Package hasn't been installed
+    pass
 
 from . import aeqdsk
 from . import geqdsk
